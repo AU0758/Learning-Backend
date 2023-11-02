c25729494041af40d51d4471da0e57b2a35f1852
Watnir <137804027+AU0758@users.noreply.github.com>
2023-11-02 12:15:59 +0000

    Lesson 2

--- boilerplate-express-main/myApp.js
+++ boilerplate-express-main/myApp.js
@@ -3,11 +3,20 @@ let app = express();
 
 console.log('Hello world')
 
+indexPath = __dirname + '/views/index.html'
+stylePath = __dirname + '/public/style.css'
+
 app.get(
     '/',
     function(req, res) {
-        res.send('Hello Express');
+        res.sendFile(indexPath);
     }
+);
+app.use(
+    '/',
+    express.static(stylePath)
 )
 
+
+res.sendFile(stylePath);
 module.exports = app;
