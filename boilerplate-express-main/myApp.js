f377e7bc75edaff4b158602a2e2bc2c7e9fe9052
Watnir <137804027+AU0758@users.noreply.github.com>
2023-11-02 09:22:11 +0000

    Lesson 2

---
+++ boilerplate-express-main/myApp.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let app = express();
+
+console.log('Hello world')
+
+app.get(
+    '/',
+    function(req, res) {
+        res.send('Hello Express');
+    }
+)
+
+module.exports = app;
